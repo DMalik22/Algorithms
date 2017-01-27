930cff9d1602e65f97cd4ca7dd71884a5f841202
Deeptanshu Malik <DeeptanshuM@users.noreply.github.com>
2017-01-26 23:40:57 -0500

    Hackerrank, CTCI problem

---
+++ rotateArrayLeft.cpp
@@ -0,0 +1,58 @@
+/*
+input:
+n k
+array of n ints to be rotate left k places
+
+my solution:
+simply utilize the modulo property that anynumber mod n guarantess that
+the result will be between 0 and n - 1, both ends inclusive, iterate throught the
+given array and make the appropriate assignments
+*/
+
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+vector<int> array_left_rotation(vector<int> a, int n, int k) {
+    vector<int> to_ret (n);
+    for(int i = 0; i < a.size(); i++){
+        to_ret[i] = a[(k + i) % n];
+    }
+    return to_ret;
+}
+
+int main(){
+    int n;
+    int k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for(int a_i = 0;a_i < n;a_i++){
+        cin >> a[a_i];
+    }
+    vector<int> output = array_left_rotation(a, n, k);
+    for(int i = 0; i < n;i++)
+        cout << output[i] << " ";
+    cout << endl;
+    return 0;
+}
